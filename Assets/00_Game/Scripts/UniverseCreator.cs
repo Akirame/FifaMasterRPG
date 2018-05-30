7952d2fd4c43ad3772cd4a3ae9c912761d0746bc
Akirame <gsvillalba12@hotmail.com>
2018-05-30 20:33:52 -0300

    Add UniverseCreator & Planet

---
+++ Assets/00_Game/Scripts/UniverseCreator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniverseCreator : MonoBehaviourSingleton<UniverseCreator>
+{
+    public GameObject planetPrefab;
+
+    public float minPlanetSize;
+    public float maxPlanetSize;
+
+    public float minPlanetDistanceToSun;
+    public float maxPlanetDistanceToSun;
+
+    public int minPlanetsToCreate;
+    public int maxPlanetsToCreate;
+
+    public GameObject Sun;
+
+    public void CreateUniverse()
+    {
+        int planetCount = Random.Range(minPlanetsToCreate, maxPlanetsToCreate);
+
+        for (int i = 0; i < planetCount; i++)
+        {
+            GameObject newPlanetGO = Instantiate(planetPrefab);
+            Planet newPlanet = newPlanetGO.GetComponent<Planet>();
+
+            float distanceToSun = Random.Range(minPlanetDistanceToSun, maxPlanetDistanceToSun);
+
+            float planetSize = Random.Range(minPlanetSize, maxPlanetSize);
+
+            newPlanet.Set(Sun, distanceToSun, planetSize);
+            newPlanet.traslationSpeed = Random.Range(5f, 20f);
+            newPlanet.transform.parent = transform;
+        }
+    }
+}
