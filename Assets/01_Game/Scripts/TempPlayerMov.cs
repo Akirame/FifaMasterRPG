d4f89197dd629504e59cc416203a298f423033cc
Beto <betocastro1819@gmail.com>
2018-07-01 13:21:08 -0300

    Player basic movement

---
+++ Assets/01_Game/Scripts/TempPlayerMov.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayerMov : MonoBehaviour
+{
+	public float speed = 5f;
+
+	void Update ()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector3 mov = transform.position;
+
+		mov.x += horizontal * speed * Time.deltaTime;
+		mov.z += vertical * speed * Time.deltaTime;
+
+		transform.position = mov;
+	}
+}
