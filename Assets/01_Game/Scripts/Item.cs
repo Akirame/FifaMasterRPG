20a260475d8560faf7be893fb716864e7329ff18
Beto <betocastro1819@gmail.com>
2018-07-01 14:25:38 -0300

    Item interaction trigger

---
+++ Assets/01_Game/Scripts/Item.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Iventory/Item")]
+public class Item : ScriptableObject
+{
+	new public string name = "Item name";
+	public Sprite icon = null;
+}
---
+++ Assets/01_Game/Scripts/ItemPickUp.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ItemPickUp : Interactable
+{
+	void Start()
+	{
+		AddSphereTrigger();
+	}
+
+	public override void Interact()
+	{
+		base.Interact(); // Executes Interact() inside Interactable class
+
+		PickUp();
+	}
+
+	void PickUp()
+	{
+		Debug.Log("Press B to pick up " + name);
+		// Add item to inventory
+	}
+
+	void OnTriggerEnter(Collider player)
+	{
+		if (player.tag == "Player")
+			Interact();
+	}
+
+	void AddSphereTrigger()
+	{
+		gameObject.AddComponent<SphereCollider>();
+		SphereCollider trigger = GetComponent<SphereCollider>();
+		trigger.isTrigger = true;
+		trigger.radius = radius;
+	}
+}
