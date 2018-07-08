8ec6652a8e04d814bb350fae95e9d8193e28bec9
Akirame <gsvillalba12@hotmail.com>
2018-07-08 12:07:09 -0300

    More control with the ball

--- Assets/01_Game/Scripts/TempPlayerMov.cs
+++ Assets/01_Game/Scripts/TempPlayerMov.cs
@@ -10,8 +10,24 @@ public class TempPlayerMov : MonoBehaviour
     public Vector3 direction;
     public float rotationSpeed = 30f;
     public bool ballInControl = false;
+    public float ballForceForward;
+    public float ballForceUpward;
+    public float maxForceUp;
+    public float maxForceForw;
 
-	void Update ()
+    private float distanceBallFromPlayer;
+    private float forceUp;
+    private float forceForw;    
+    private bool shooting;    
+
+    private void Start()
+    {
+        forceUp = 0;
+        forceForw = 0;        
+        shooting = false;        
+        distanceBallFromPlayer = 1;
+    }
+    void Update ()
 	{
 		float horizontal = Input.GetAxis("Horizontal");
 		float vertical = Input.GetAxis("Vertical");
@@ -34,7 +50,7 @@ public class TempPlayerMov : MonoBehaviour
             ballPos.y = ball.transform.position.y;
             ballPos.x = transform.position.x;
             ballPos.z = transform.position.z;
-            ball.transform.position = ballPos + transform.forward * 1;
+            ball.transform.position = ballPos + transform.forward * distanceBallFromPlayer;
         }
     }
 
@@ -46,10 +62,28 @@ public class TempPlayerMov : MonoBehaviour
 
     private void ShootBehaviour()
     {
-        if (ball && Input.GetKeyDown(KeyCode.Space))
+        if (ball && Input.GetKey(KeyCode.Space) & !shooting)
         {
+            forceForw += ballForceForward * Time.deltaTime;
+            forceUp += ballForceUpward * Time.deltaTime;
+            if (forceForw >= maxForceForw)
+                forceForw = maxForceForw;
+            if (forceUp >= maxForceUp)
+                forceUp = maxForceUp;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            shooting = true;
+        }
+        if (ball && shooting)
+        {
+            Vector3 dir =  (transform.forward * forceForw) + (transform.up * forceUp);
             ballInControl = false;
-            ball.GetComponent<Ball>().Shoot(transform.forward);
+            Debug.Log(dir);
+            ball.GetComponent<Ball>().Shoot(dir);
+            forceForw = 0;
+            forceUp = 0;
+            shooting = false;
         }
     }
 
